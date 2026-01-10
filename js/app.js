a537db80473d0948764d0b62dfbe09954f83b8ad
LakshyaSenthilkumar <vanithasenthilkumar880@gmail.com>
2026-01-10 19:53:13 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,22 +1,31 @@
 function placeOrder(item) {
-  const name = prompt("Enter your name:");
-  const quantity = prompt("Enter quantity:");
-  const address = prompt("Enter delivery address:");
+    const name = prompt("Enter your name:");
+    const quantity = prompt("Enter quantity:");
+    const address = prompt("Enter delivery address:");
 
-  if (!name || !quantity || !address) {
-    alert("All fields are required!");
-    return;
-  }
+    if (!name || !quantity || !address) {
+        alert("All fields are required!");
+        return;
+    }
 
-  fetch("http://backend-alb-432079772.us-east-1.elb.amazonaws.com7/backend-app/insert.php", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/x-www-form-urlencoded"
-    },
-    body: `name=${name}&product=${item}&quantity=${quantity}&address=${address}`
-  })
-  .then(res => res.text())
-  .then(data => alert(data))
-  .catch(() => alert("Order failed"));
+    const bodyData = new URLSearchParams({
+        name: name,
+        product: item,
+        quantity: quantity,
+        address: address
+    });
+
+    fetch("http://backend-alb-432079772.us-east-1.elb.amazonaws.com/backend-app/insert.php", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/x-www-form-urlencoded"
+        },
+        body: bodyData.toString()
+    })
+    .then(res => res.text())
+    .then(data => alert(data))
+    .catch(err => {
+        console.error(err);
+        alert("Order failed");
+    });
 }
-
